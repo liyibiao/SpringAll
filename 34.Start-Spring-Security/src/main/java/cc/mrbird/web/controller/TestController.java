771b40f9f22ff032831538f960e7a1379cb0a79d
liyibiao <943968202@qq.com>
2020-03-20 17:29:23 +0800

    security test1

--- 34.Start-Spring-Security/src/main/java/cc/mrbird/web/controller/TestController.java
+++ 34.Start-Spring-Security/src/main/java/cc/mrbird/web/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("hello")
     public String hello() {
+
         return "hello spring security";
     }
 }
